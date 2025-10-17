d29f138ebcac7af0e15cb9a2783f6cf03bf9b811
Mirela Schneider Bianchi <mirela.bianchi@sou.inteli.edu.br>
2025-10-17 09:07:21 -0300

    Add LED blinking setup and loop functions
    Implement basic LED blinking functionality.

--- projeto.ino
+++ projeto.ino
@@ -1,1 +1,10 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
 
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH); // acende
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW); // apaga
+  delay(1000);
+}
